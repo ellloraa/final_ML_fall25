7cd16d6e81a944671df5873d2c9002bf748744b3
Ellora Devulapally <ellora.devulapally@duke.edu>
2025-11-16 17:22:30 -0500

    hello!

--- demo.js
+++ demo.js
@@ -1,14 +1,13 @@
 import SidechatAPIClient from "./src/classes/SidechatAPIClient.js";
 
-const api = new SidechatAPIClient();
+const api = new SidechatAPIClient("eyJhbGciOiJIUzI1NiJ9.YWQ5MWFkMzEtNTk0OS00NTVkLWJhYmQtZDJmYjI1Y2IyMDBl.-AANbJDBG03rh1c_RGCvb70CL__Qu0_oSH5r5PKeYTg");
 
-async function login() {
-  const phoneNumber = "YOUR_PHONE_HERE";
-
-  console.log("Sending SMS...");
-  await api.loginViaSMS(phoneNumber);
-
-  console.log("Enter the code you got in the SMS and rerun this file with it.\n");
+async function main() {
+  // Now you're authenticated forever (until token expires)
+  const groups = await api.searchAvailableGroups("Davidson");
+  const groupID = "1fcad7b1-fce2-4ae1-bd48-bd1917b62d98";
+  const posts = await api.getGroupPosts(groupID, "recent");
+  console.log(posts);
 }
 
-login();
+main();
